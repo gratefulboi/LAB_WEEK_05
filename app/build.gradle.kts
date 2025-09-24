715f456fb66634a322fde2dbeb84f5cb33d9ea72
gratefulboi <bryanyung2005@gmail.com>
2025-09-24 15:47:56 +0700

    Commit No. 1 - Retrofit Setup and Fetch API JSON Response

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,5 +45,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
 
 }
